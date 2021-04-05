6547f07bf47e92fc48ff8eb625b36b0e6b5861cd
SandroAlmeidaDev <sandro.almeida.silva17@gmail.com>
2021-04-05 00:53:14 -0300

    Refactored code into components

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,20 +1,22 @@
-import { Avatar, Box, Flex, Text } from '@chakra-ui/react'
+import { Avatar, Box, Flex, Stack, Text } from '@chakra-ui/react'
 
 export function Profile() {
   return (
     <Flex align="center">
-      <Box>
-        <Text>Sandro Almeida</Text>
-        <Text color="gray.300" fontSize="small">
-          sandro.almeida.silva17@gmail.com
-        </Text>
-      </Box>
+      <Stack direction="row" spacing="4" justify="space-between" align="center">
+        <Box>
+          <Text>Sandro Almeida</Text>
+          <Text color="gray.300" fontSize="small">
+            sandro.almeida.silva17@gmail.com
+          </Text>
+        </Box>
 
-      <Avatar
-        size="md"
-        name="Sandro Almeida"
-        src="https://avatars.githubusercontent.com/u/58606794?v=4"
-      />
+        <Avatar
+          size="md"
+          name="Sandro Almeida"
+          src="https://avatars.githubusercontent.com/u/58606794?v=4"
+        />
+      </Stack>
     </Flex>
   )
 }
